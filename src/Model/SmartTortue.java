75096d9b94b382f126affa2ac29ac3f71fe06257
Aceitix <gaetan.roche@etu.univ-lyon1.fr>
2016-06-05 19:06:49 +0200

    complétion du flocking reste deux fonction à faire marqué en TO DO

--- src/Model/SmartTortue.java
+++ src/Model/SmartTortue.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by gaetan on 05/06/16.
@@ -8,15 +9,55 @@ import java.util.ArrayList;
 public class SmartTortue extends SimpleTortue {
     private int angle;
     private int viewDist;
+    private int vitesse;
+    private int distMin;
 
-    public SmartTortue(int _width, int _height, int angle, int viewDist) {
+    public SmartTortue(int _width, int _height, int angle, int viewDist, int vitesse, int distMin) {
         super(_width, _height);
         this.angle = angle;
         this.viewDist = viewDist;
+        this.vitesse = vitesse;
+        this.distMin = distMin;
     }
 
 
     public void moveFlocking(ArrayList<SmartTortue> tortues) {
+        int vitesseMoy = 0;
+        int dirMoy = 0;
+        int i = 0;
+        for (SmartTortue t: tortues) {
+            if(estDansChampsVision(t)){
+                dirMoy += t.getDir();
+                vitesseMoy += t.getVitesse();
+                i++;
+            }
+        }
+        if(i !=0) {
+            setVitesse(vitesseMoy / i);
+            setDir(dirMoy / i);
+        }
+        else{
+            Random rand = new Random();
+            setDir(rand.nextInt(360));
+            setVitesse(rand.nextInt(50));
+        }
 
+        //maintenant on check qu'on est a une distance minimal des autres tortues
+        for (SmartTortue t : tortues) {
+            //TO DO
+        }
+    }
+
+    public boolean estDansChampsVision(SmartTortue t){
+        return true;
+        //TO DO
+    }
+
+    public int getVitesse() {
+        return vitesse;
+    }
+
+    public void setVitesse(int vitesse) {
+        this.vitesse = vitesse;
     }
 }
